54dc325fb02d5e5eac0dd7f53bbeb94cd116f0ac
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-11-07 23:27:16 -0500

    A few more UnitTests

--- src/test/java/baseline/GUIControllerTest.java
+++ src/test/java/baseline/GUIControllerTest.java
@@ -1,24 +1,61 @@
 package baseline;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GUIControllerTest {
 
     @Test
-    void addItem() {
-        Item testItem = new Item(null,"NewTitle", null, false);
+    void demoTestMethod() {
+        assertTrue(true);
+    }
 
+    @Test
+    void addItem() {
+        Item testItem = new Item(null,"NewTitle1", null, false);
+        Item testItem2 = new Item(null,"NewTitle2", null, false);
+        Item testItem3 = new  Item(null,"NewTitle3", null, false);
+
+        ObservableList<Item> iList = FXCollections.observableArrayList();
+        iList.setAll(testItem,testItem2, testItem3);
+
+        if (iList != null){
+            assertTrue(true);
+        }
     }
 
     @Test
     void deleteItem() {
+        Item testItem = new Item(null,"NewTitle1", null, false);
+
+        ObservableList<Item> iList = FXCollections.observableArrayList();
+        iList.setAll(testItem);
+
+        iList.remove(testItem);
+
+        if (iList == null){
+            assertTrue(true);
+        }
     }
 
     @Test
     void clearItemList() {
+        Item testItem = new Item(null,"NewTitle1", null, false);
+        Item testItem2 = new Item(null,"NewTitle2", null, false);
+        Item testItem3 = new  Item(null,"NewTitle3", null, false);
+
+        ObservableList<Item> iList = FXCollections.observableArrayList();
+        iList.setAll(testItem,testItem2, testItem3);
+
+        iList.clear();
+
+        if (iList == null){
+            assertTrue(true);
+        }
     }
 }
